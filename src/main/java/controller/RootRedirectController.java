0e93e11e06f6639ef66d9874ee36d148816879d7
yunseong <ttttma2@naver.com>
2025-01-10 07:05:37 +0900

    Initial commit for boardApp

---
+++ src/main/java/controller/RootRedirectController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/") // 루트 경로 매핑
+public class RootRedirectController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(req.getContextPath() + "/login"); // /login으로 리다이렉트
+    }
+}
