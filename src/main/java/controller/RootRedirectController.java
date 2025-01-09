b902b367cc8876888f356428ea7844ad9b8bb4bb
yunseong <ttttma2@naver.com>
2025-01-10 07:47:02 +0900

    Update web.xml and build settings for Heroku deployment

--- src/main/java/controller/RootRedirectController.java
+++ src/main/java/controller/RootRedirectController.java
@@ -1,13 +1,11 @@
 package controller;
 
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/") // 루트 경로 매핑
 public class RootRedirectController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
